dd6208e4cd959f7899ab4abe82d5dcdce3149c33
CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
2026-05-22 18:30:50 -0300

    EquipamentoController

--- src/main/java/org/example/controller/AlunoController.java
+++ src/main/java/org/example/controller/AlunoController.java
@@ -1,4 +1,19 @@
 package org.example.controller;
 
+import org.example.entity.Aluno;
+import org.example.service.AlunoService;
+
 public class AlunoController {
+
+    private AlunoService alunoService = new AlunoService();
+
+    public void cadastrarAluno(String nome, String matricula) {
+        if (nome.isBlank()) {throw new RuntimeException("Erro: nome em branco");}
+        if (nome.length()< 3) {throw new RuntimeException("Erro nome invalido");}
+        if (matricula.isBlank()){throw new RuntimeException("Erro: matricula em branco");}
+        if (!matricula.matches("[a-zA-Z0-9]+")){throw new RuntimeException("Erro: matricula invalidas");}
+
+        alunoService.cadastrarAluno(nome,matricula);
+
+    }
 }
