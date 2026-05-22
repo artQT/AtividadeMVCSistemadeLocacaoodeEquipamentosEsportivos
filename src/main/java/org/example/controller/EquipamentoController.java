5ada30770740d6b2092a13fdff1b240e67c3d318
CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
2026-05-22 20:03:53 -0300

    EquipamentoController/LocacaoController

--- src/main/java/org/example/controller/EquipamentoController.java
+++ src/main/java/org/example/controller/EquipamentoController.java
@@ -7,10 +7,10 @@ public class EquipamentoController {
     private EquipamentoService equipamentoService = new EquipamentoService();
 
     public void cadastrarEqupamento(String nome, String tipo){
-        if(nome.isBlank()){throw new RuntimeException("Erro: nome em branco");}
-        if(nome.length()< 3){throw new RuntimeException("Erro: nome invalido");}
-        if(tipo.isBlank()){throw new RuntimeException(("Erro: tipo em branco"));}
-        if(tipo.matches("[a-zA-Z ]+"))throw new RuntimeException("Erro:ipo invalido");}
+        if(nome.isBlank()){throw new IllegalArgumentException("Erro: nome em branco");}
+        if(nome.length()< 3){throw new IllegalArgumentException("Erro: nome invalido");}
+        if(tipo.isBlank()){throw new IllegalArgumentException(("Erro: tipo em branco"));}
+        if(tipo.matches("[a-zA-Z ]+"))throw new IllegalArgumentException("Erro:ipo invalido");}
 
     equipamentoService.cadastrarEquipamento(nome, tipo);
 }
---
+++ src/main/java/org/example/controller/LocacaoController.java
@@ -0,0 +1,17 @@
+package org.example.controller;
+
+import org.example.entity.*;
+import org.example.service.LocacaoService;
+
+public class LocacaoController {
+
+
+    LocacaoService locacaoService = new LocacaoService();
+
+    public void realizarLocacao(int id, Aluno aluno, Equipamento equipamento,String dataLocacao, String status){
+        if(id <= 0){throw new IllegalArgumentException("Erro: ID ilvalido");}
+        if(aluno == null)throw new IllegalArgumentException("Erro: Aluno não encontrado ");}
+        if(equipamento == null)
+    }
+
+}
